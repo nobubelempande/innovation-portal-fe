a2f549a91147a7a029701c39f4d396b8570a0280
Nobu Mpande <nobu.mpande@entelect.co.za>
2024-04-24 19:05:57 +0200

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import './App.css';
@@ -44,8 +45,8 @@ function App() {
   };
 
   return (
-    <div style={{ backgroundColor: '#990f02', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
-      <div style={{ backgroundColor: '#FF7F7F', borderRadius: '24px', margin: '24px', padding: '24px', maxWidth: '600px', width: '100%' }}>
+    <div style={{ backgroundColor: '#B01116', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ backgroundColor: '#FF7F7F', borderRadius: '24px', margin: '24px', padding: '24px', maxWidth: '600px', width: '100%', }}>
         <div>
           <span style={{ display: 'flex', justifyContent: 'center' }}>
             <p style={{ color: 'white', paddingRight: '2px' }}>Already have an account?</p>
@@ -61,37 +62,40 @@ function App() {
             <Form style={{ display: 'flex', flexDirection: 'column' }}>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="name">Name</label>
-                <Field type="text" id="name" name="name" />
+                <Field className="Field" type="text" id="name" name="name" placeholder="Your name" />
                 <ErrorMessage name="name" component="div" />
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="surname">Surname</label>
-                <Field type="text" id="surname" name="surname" />
+                <Field type="text" id="surname" name="surname" placeholder="Your surname" className="Field" />
                 <ErrorMessage name="surname" component="div" />
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="email">Email Address</label>
-                <Field type="email" id="email" name="email" />
+                <Field type="email" id="email" name="email" placeholder="Your email address" className="Field" />
                 <ErrorMessage name="email" component="div" />
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="number">Phone number</label>
-                <Field type="text" id="number" name="number" />
+                <Field type="text" id="number" name="number" placeholder="000-000-0000" className="Field" />
                 <ErrorMessage name="number" component="div" />
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="password">Password</label>
-                <Field type="text" id="password" name="password" />
+                <Field type="text" id="password" name="password" placeholder="Password" className="Field" />
                 <ErrorMessage name="password" component="div" />
               </div>
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '16px' }}>
                 <label htmlFor="confirm">Confirm Password</label>
-                <Field type="text" id="confirm" name="confirm" />
+                <Field type="text" id="confirm" name="confirm" placeholder="Retype Password" className="Field" />
                 <ErrorMessage name="confirm" component="div" />
               </div>
-              <button type="submit" disabled={isSubmitting}>
-                Register
-              </button>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+                <Button style={{ backgroundColor: "#B01116", color: 'white', width: '178px', height: '48px' }} type="submit" disabled={isSubmitting}>
+                  Register
+                </Button>
+              </div>
+
             </Form>
           )}
         </Formik>
